41f95e45e0cf035f68276e75be942d0cdc56ae6f
Dinuka Mudalige <m.dinuka@gtngroup.com>
2023-08-22 11:08:02 +0530

    Updated the branch 'using-Runtime'

--- src/main/java/com/gtngroup/memtrack/MemoryTracker.java
+++ src/main/java/com/gtngroup/memtrack/MemoryTracker.java
@@ -2,7 +2,6 @@ package com.gtngroup.memtrack;
 
 import org.springframework.stereotype.Component;
 
-//import java.lang.management.ManagementFactory;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -15,21 +14,15 @@ public class MemoryTracker {
 
     public void trackMemoryUsage() {
         long totalMemory = 0;
-//        String totalMemString = "0";
         int count = 0;
         while (totalMemory < TARGET_MEMORY_BYTES) {
-//        while (Long.parseLong(totalMemString) < TARGET_MEMORY_BYTES) {
-                // String randomString = UUID.randomUUID().toString();
-                // stringList.add(randomString);
                 stringList.add(UUID.randomUUID().toString());
                 System.out.println(stringList.get(count));
                  totalMemory = getUsedMemory();
-                // totalMemString = getUsedMemory();
                 // System.gc(); // Explicitly call garbage collector after each add
                 count++;
         }
          System.out.println("Total memory used: " + Long.toString(totalMemory));
-        // System.out.println("Total memory used: " + totalMemString);
         System.out.println("Number of elements in the list: " + stringList.size());
     }
 
@@ -37,8 +30,4 @@ public class MemoryTracker {
         // Using Runtime to estimate used heap memory
         return Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
     }
-
-//    private String getUsedMemory() {
-//        return ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().toString();
-//    }
 }
